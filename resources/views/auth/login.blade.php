cd02798d111bf4f2a7afdd91a93b1749e9a49957
Diego <diegommorais12@gmail.com>
2019-10-25 12:23:46 -0300

    ajustes no tamplate e dashboard

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,89 +1,97 @@
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
 <!DOCTYPE html>
 <html lang="en">
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    
-    <!--Fontawesome CDN-->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+	<!--Fontawesome CDN-->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-	<!--Custom styles-->
-	<link rel="stylesheet" type="text/css" href="styles.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-    <link href="{{ asset('css/login.css') }}" rel="stylesheet">
+
+	<link href="{{ asset('css/login.css') }}" rel="stylesheet">
 </head>
-    <title>Document</title>
+<title>Document</title>
 </head>
+
 <body>
-<div class="container">
-	<div class="d-flex justify-content-center h-100">
-		<div class="card">
-			<div class="card-header">
-				<h3>Sign In</h3>
-				<div class="d-flex justify-content-end social_icon">
-					<span><i class="fab fa-facebook-square"></i></span>
-					<span><i class="fab fa-google-plus-square"></i></span>
-					<span><i class="fab fa-twitter-square"></i></span>
-				</div>
-			</div>
-			<div class="card-body">
-				<form method="POST" action="{{ route('login') }}">
-                @csrf
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-user"></i></span>
-						</div>
-						<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-						
-                        @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-					<div class="input-group form-group">
-						<div class="input-group-prepend">
-							<span class="input-group-text"><i class="fas fa-key"></i></span>
-						</div>
-						<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                        @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                        @enderror
-                    </div>
-					<div class="row align-items-center remember">
-						<input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>Remember Me
+	<div class="nav top-right links d-flex justify-content-center">
+		<a href="/">Incio</a>
+		<a href="{{route('register')}}"> Cadastro</a>
+
+	</div>
+	<div class="container ">
+		<div class="d-flex justify-content-center   h-100">
+			<div class="card">
+				<div class="card-header">
+					<h3>Sign In</h3>
+					<div class="d-flex justify-content-end social_icon">
+						<span><i class="fab fa-facebook-square"></i></span>
+						<span><i class="fab fa-google-plus-square"></i></span>
+						<span><i class="fab fa-twitter-square"></i></span>
 					</div>
-					<div class="form-group">
-						<input type="submit" value="Login" class="btn float-right login_btn">
-                    </div>
-                    @if (Route::has('password.request'))
-                        <a class="btn btn-link" href="{{ route('password.request') }}">
-                            {{ __('Forgot Your Password?') }}
-                        </a>
-                    @endif
-				</form>
-			</div>
-			<div class="card-footer">
-				<div class="d-flex justify-content-center links">
-					Don't have an account?<a href="#">Sign Up</a>
 				</div>
-				<div class="d-flex justify-content-center">
-					<a href="#">Forgot your password?</a>
+				<div class="card-body">
+					<form method="POST" action="{{ route('login') }}">
+						@csrf
+						<div class="input-group form-group">
+							<div class="input-group-prepend">
+								<span class="input-group-text"><i class="fas fa-user"></i></span>
+							</div>
+							<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+							@error('email')
+							<span class="invalid-feedback" role="alert">
+								<strong>{{ $message }}</strong>
+							</span>
+							@enderror
+						</div>
+						<div class="input-group form-group">
+							<div class="input-group-prepend">
+								<span class="input-group-text"><i class="fas fa-key"></i></span>
+							</div>
+							<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+							@error('password')
+							<span class="invalid-feedback" role="alert">
+								<strong>{{ $message }}</strong>
+							</span>
+							@enderror
+						</div>
+						<div class="row align-items-center remember">
+							<input type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>Remember Me
+						</div>
+						<div class="form-group">
+							<input type="submit" value="Login" class="btn float-right login_btn">
+						</div>
+						@if (Route::has('password.request'))
+						<a class="btn btn-link" href="{{ route('password.request') }}">
+							{{ __('Forgot Your Password?') }}
+						</a>
+						@endif
+					</form>
+				</div>
+				<div class="card-footer">
+					<div class="d-flex justify-content-center links">
+						Don't have an account?<a href="#">Sign Up</a>
+					</div>
+					<div class="d-flex justify-content-center">
+						<a href="#">Forgot your password?</a>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
-    
+	<script type="text/javascript" src="{{asset('js/sweetalerts.min.js')}}"></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </body>
-</html> 
+
+</html>
--- resources/views/relatorios/dashboard.blade.php
+++ resources/views/relatorios/dashboard.blade.php
@@ -2,38 +2,18 @@
 @section('title',' Titulo')
 @section('body')
 <div class="container h-100">
-            <div class="d-flex justify-content-center h-100">
-                <div class="searchbar">
-                    <input class="search_input" type="text" name="" placeholder="Search...">
-                    <a href="#" class="search_icon"><i class="fas fa-search"></i></a>
-                </div>
-            </div>
-        </div>
-<div class="row">
-    <div class="col-lg-3 col-md-4 col-sm-12 shadow p-3 mb-5 rounded">
-
-        <div class="card">
-
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-lg-5 col-sm-12 col-md-5">
-                        <img src="{!! asset('imgs/estoque-icon.png') !!}" alt="" height="72px" class="image image-responsive">
-
-                    </div>
-                    <div class="col-lg-7 col-md-5 col-sm-12">
-                        <p class="card-text"> Text 1</p>
-                        <h3 class="card-title">Text 2</h3>
-                    </div>
-                </div>
-            </div>
-            <div class="card-footer ">
-                <p class="text-left">Relatório de estoque</p>
-            </div>
+    <div class="d-flex justify-content-center h-100">
+        <div class="searchbar">
+            <input class="search_input" type="text" name="" placeholder="Search...">
+            <a href="#" class="search_icon"><i class="fas fa-search"></i></a>
         </div>
     </div>
+</div>
+<div class="row">
+   
     <!-- -->
-    
-    <div class="col-lg-3 col-md-4 shadow p-3 mb-5 rounded">
+
+    <div class="col-lg-4 col-md-4 shadow p-3 mb-5 rounded">
         <div class="card">
             <div class="card-body">
                 <div class="row">
@@ -52,7 +32,7 @@
             </div>
         </div>
     </div>
-    <div class="col-lg-3 col-md-4 shadow p-3 mb-5 rounded">
+    <div class="col-lg-4 col-md-4 shadow p-3 mb-5 rounded">
         <div class="card">
             <div class="card-body">
                 <div class="row">
@@ -71,7 +51,7 @@
             </div>
         </div>
     </div>
-    <div class="col-lg-3 col-md-4 shadow p-3 mb-5 rounded">
+    <div class="col-lg-4 col-md-4 shadow p-3 mb-5 rounded">
         <div class="card">
             <div class="card-body">
                 <div class="row">
@@ -90,15 +70,15 @@
             </div>
         </div>
     </div>
-  
+
 </div>
 <!-- Row 2 -->
 
 <div class="row">
 
-    <div class="col-sm-12 col-md-6  col-lg-4 shadow p-3 mb-5 rounded" >
-        <div class="card" >
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0-rc.1/Chart.min.js" integrity="sha256-qJdfkTrvMTvYJwkeb1z9a+rOErkiTyqpDz5vi7lZ7MQ=" crossorigin="anonymous"></script>
+    <div class="col-sm-12 col-md-6  col-lg-4 shadow p-3 mb-5 rounded">
+        <div class="card">
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0-rc.1/Chart.min.js" integrity="sha256-qJdfkTrvMTvYJwkeb1z9a+rOErkiTyqpDz5vi7lZ7MQ=" crossorigin="anonymous"></script>
 
             <canvas id="myChart"></canvas>
             <div class="card-body">
@@ -109,9 +89,9 @@
         </div>
     </div>
     <div class="col-sm-12 col-md-6  col-lg-4 shadow p-3 mb-5 rounded">
-        <div class="card" >
-     
-            <canvas id="myLineChart" ></canvas>
+        <div class="card">
+
+            <canvas id="myLineChart"></canvas>
             <div class="card-body">
                 <h5 class="card-title">Numero de clientes</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -120,8 +100,8 @@
         </div>
     </div>
     <div class="col-sm-12 col-md-6  col-lg-4">
-        <div class="card" >
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0-rc.1/Chart.min.js" integrity="sha256-qJdfkTrvMTvYJwkeb1z9a+rOErkiTyqpDz5vi7lZ7MQ=" crossorigin="anonymous"></script>
+        <div class="card">
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0-rc.1/Chart.min.js" integrity="sha256-qJdfkTrvMTvYJwkeb1z9a+rOErkiTyqpDz5vi7lZ7MQ=" crossorigin="anonymous"></script>
 
             <canvas id="myPieChart"></canvas>
             <div class="card-body">
@@ -131,7 +111,7 @@
             </div>
         </div>
     </div>
- 
+
 </div>
 <div class="row ">
     <div class="col-sm-12 col-md-12 col-lg-6 shadow p-3 mb-5 rounded">
@@ -141,7 +121,7 @@
             </div>
             <div class="card-body">
                 <table class="table text-center table-striped border    ">
-                    <thead class="thead-dark"> 
+                    <thead class="thead-dark">
                         <tr>
                             <th>Marcar</th>
                             <th>Descrição</th>
@@ -151,29 +131,29 @@
                     <tbody>
                         <tr>
                             <td><input type="checkbox"></td>
-                            <td>Lorem ipsum dolor sit  elit. Sed, debitis praesentium. Impedit, nisi dolor est nemo </td>
-                            <td  class="btn-group" role="group" aria-label="...">
-                              
-                                <a   href="#" class="btn "><i class="far fa-edit"></i></a>
-                                <a   href="#" class="btn "><i class="far fa-trash-alt" style="color:red"></i></a>
+                            <td>Lorem ipsum dolor sit elit. Sed, debitis praesentium. Impedit, nisi dolor est nemo </td>
+                            <td class="btn-group" role="group" aria-label="...">
+
+                                <a href="#" class="btn "><i class="far fa-edit"></i></a>
+                                <a href="#" class="btn "><i class="far fa-trash-alt" style="color:red"></i></a>
                             </td>
                         </tr>
                         <tr>
                             <td><input type="checkbox"></td>
-                            <td>Lorem ipsum dolor sit  elit. Sed, debitis praesentium. Impedit, nisi dolor est nemo </td>
-                            <td  class="btn-group" role="group" aria-label="...">
-                              
-                                <a   href="#" class="btn "><i class="far fa-edit"></i></a>
-                                <a   href="#" class="btn "><i class="far fa-trash-alt" style="color:red"></i></a>
+                            <td>Lorem ipsum dolor sit elit. Sed, debitis praesentium. Impedit, nisi dolor est nemo </td>
+                            <td class="btn-group" role="group" aria-label="...">
+
+                                <a href="#" class="btn "><i class="far fa-edit"></i></a>
+                                <a href="#" class="btn "><i class="far fa-trash-alt" style="color:red"></i></a>
                             </td>
                         </tr>
                         <tr>
                             <td><input type="checkbox"></td>
-                            <td>Lorem ipsum dolor sit  elit. Sed, debitis praesentium. Impedit, nisi dolor est nemo </td>
-                            <td  class="btn-group" role="group" aria-label="...">
-                              
-                                <a   href="#" class="btn "><i class="far fa-edit"></i></a>
-                                <a   href="#" class="btn "><i class="far fa-trash-alt" style="color:red"></i></a>
+                            <td>Lorem ipsum dolor sit elit. Sed, debitis praesentium. Impedit, nisi dolor est nemo </td>
+                            <td class="btn-group" role="group" aria-label="...">
+
+                                <a href="#" class="btn "><i class="far fa-edit"></i></a>
+                                <a href="#" class="btn "><i class="far fa-trash-alt" style="color:red"></i></a>
                             </td>
                         </tr>
                     </tbody>
@@ -188,7 +168,7 @@
             </div>
             <div class="card-body">
                 <table class="table text-center table-striped border ">
-                    <thead class="thead-light"> 
+                    <thead class="thead-light">
                         <tr>
                             <th>Nome</th>
                             <th>Salário</th>
@@ -199,14 +179,14 @@
                         <tr>
                             <td>John Doe</td>
                             <td>$ 6.738</td>
-                            <td> Analísta de Sistemas     </td>
+                            <td> Analísta de Sistemas </td>
                         </tr>
                         <tr>
                             <td>Maria Joana</td>
                             <td>$ 4.738</td>
-                            <td> Programadaora php junior     </td>
+                            <td> Programadaora php junior </td>
                         </tr>
-                       
+
                     </tbody>
                 </table>
             </div>
@@ -216,145 +196,148 @@
 @endsection
 @yield('css')
 <style>
- .searchbar{
-    margin-bottom: auto;
-    margin-top: auto;
-    height: 60px;
-    background-color: #3a3f48;
-    border-radius: 30px;
-    padding: 10px;
+    .searchbar {
+        margin-bottom: auto;
+        margin-top: auto;
+        height: 60px;
+        
+        background: linear-gradient(0deg, #0940af, 30%, #010e15 70%);
+
+        border-radius: 30px;
+        padding: 10px;
     }
 
-    .search_input{
-    color: white;
-    border: 0;
-    outline: 0;
-    background: none;
-    width: 0;
-    caret-color:transparent;
-    line-height: 40px;
-    transition: width 0.4s linear;
+    .search_input {
+        color: white;
+        border: 0;
+        outline: 0;
+        background: none;
+        width: 0;
+        caret-color: transparent;
+        line-height: 40px;
+        transition: width 0.4s linear;
     }
 
-    .searchbar:hover > .search_input{
-    padding: 0 10px;
-    width: 450px;
-    caret-color:red;
-    transition: width 0.4s linear;
+    .searchbar:hover>.search_input {
+        padding: 0 10px;
+        width: 450px;
+        caret-color: red;
+        transition: width 0.4s linear;
     }
 
-    .searchbar:hover > .search_icon{
-    background: white;
-    color: #e74c3c;
+    .searchbar:hover>.search_icon {
+        background: white;
+        color: #e74c3c;
     }
 
-    .search_icon{
-    height: 40px;
-    width: 40px;
-    float: right;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 50%;
-    color:white;
+    .search_icon {
+        height: 40px;
+        width: 40px;
+        float: right;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        color: white;
     }
 </style>
 @yield('js')
 <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
 
 <script>
-$(document).ready(function(){
-    pieChart()
-    barChart()
-    lineChart()
-    function lineChart(){
-        var ctx = document.getElementById('myLineChart').getContext('2d');
-        var chart = new Chart(ctx, {
-            // The type of chart we want to create
-            type: 'line',
+    $(document).ready(function() {
+        pieChart()
+        barChart()
+        lineChart()
 
-            // The data for our dataset
-            data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                datasets: [{
-                    label: 'My First dataset',
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: [0, 10, 15, 12, 18, 21, 35]
-                }]
-            },
+        function lineChart() {
+            var ctx = document.getElementById('myLineChart').getContext('2d');
+            var chart = new Chart(ctx, {
+                // The type of chart we want to create
+                type: 'line',
 
-    // Configuration options go here
-    options: {}
-});
-    }
-   function pieChart(){
-    var ctx = document.getElementById('myPieChart');
-    var myPieChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels:['Produto 1','Produto 2', 'Produto 3', 'Produto 4'],
-        datasets:[{
-            label:'receita mes',
-            data:[10,12,14,20],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.9)',
-                'rgba(54, 162, 235, 0.9)',
-                'rgba(255, 206, 86, 0.9)',
-                'rgba(75, 192, 192, 0.9)',
-            ],
-            borderColor:[
-                'rgba(255, 99, 132, 0.9)',
-                'rgba(54, 162, 235, 0.9)',
-                'rgba(255, 206, 86, 0.9)',
-                'rgba(75, 192, 192, 0.9)',
+                // The data for our dataset
+                data: {
+                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                    datasets: [{
+                        label: 'My First dataset',
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        data: [0, 10, 15, 12, 18, 21, 35]
+                    }]
+                },
 
-            ],
-            borderWidth:1
-        }]
-    }
-});
-   }
+                // Configuration options go here
+                options: {}
+            });
+        }
 
-function barChart(){
-    var ctx = document.getElementById('myChart');
-    var myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: ' Ultimos 6 meses',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.9)',
-                    'rgba(54, 162, 235, 0.9)',
-                    'rgba(255, 206, 86, 0.9)',
-                    'rgba(75, 192, 192, 0.9)',
-                    'rgba(153, 102, 255, 0.9)',
-                    'rgba(255, 159, 64, 0.9)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
+        function pieChart() {
+            var ctx = document.getElementById('myPieChart');
+            var myPieChart = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    labels: ['Produto 1', 'Produto 2', 'Produto 3', 'Produto 4'],
+                    datasets: [{
+                        label: 'receita mes',
+                        data: [10, 12, 14, 20],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.9)',
+                            'rgba(54, 162, 235, 0.9)',
+                            'rgba(255, 206, 86, 0.9)',
+                            'rgba(75, 192, 192, 0.9)',
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 0.9)',
+                            'rgba(54, 162, 235, 0.9)',
+                            'rgba(255, 206, 86, 0.9)',
+                            'rgba(75, 192, 192, 0.9)',
+
+                        ],
+                        borderWidth: 1
+                    }]
+                }
+            });
+        }
+
+        function barChart() {
+            var ctx = document.getElementById('myChart');
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    datasets: [{
+                        label: ' Ultimos 6 meses',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.9)',
+                            'rgba(54, 162, 235, 0.9)',
+                            'rgba(255, 206, 86, 0.9)',
+                            'rgba(75, 192, 192, 0.9)',
+                            'rgba(153, 102, 255, 0.9)',
+                            'rgba(255, 159, 64, 0.9)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
                     }
-                }]
-            }
+                }
+            });
         }
     });
-}
-});
-
 </script>
